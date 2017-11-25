844c9816771c03962fcaec7f2a2d1af5743df9d6
Jian-Hua, He <game09116@gmail.com>
2017-11-25 12:20:20 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,12 +28,22 @@ function rollback {
 # Install .bash_profile
 cp ../src/.bash_profile ~/
 
-# Create vim configurate
+# Setup vim install env
 mkdir ~/.vim
-cp ../src/.vimrc ~/
+cp ../src/.vimrc_install ~/.vimrc
+cp -r ../src/.vim ~/
 
-# Install package for vim plug
+# Install Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install vim package
 vim +PlugInstall +qall
 
-# Install package for vundle
 vim +PluginInstall +qall
+
+# Add vim configurate
+cp ../src/.vimrc ~/.vimrc
